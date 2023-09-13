fd3c8360a59c3a59d2f997728cb19da3ed7c2f8d
navneet9971 <navneetjha2012@gmail.com>
2023-09-13 11:12:11 +0530

    done

--- src/component/auth/NewPassword.jsx
+++ src/component/auth/NewPassword.jsx
@@ -7,9 +7,10 @@ import "./NewPassword.css"; // Import your CSS file for styling
 function NewPassword() {
   const [formData, setFormData] = useState({
     token: "",
-    password1: "", // Updated name to match the input field
-    password2: "", // Updated name to match the input field
+    password1: "",
+    password2: "",
   });
+  const [showPassword, setShowPassword] = useState(false); // State to control password visibility
   const history = useHistory();
 
   const handleInputChange = (event) => {
@@ -17,23 +18,22 @@ function NewPassword() {
     setFormData({ ...formData, [name]: value });
   };
 
+  const handlePasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     try {
-      // Make an API request to send the new password
       const response = await axiosInstance.post("password-reset/confirm/", formData);
 
-      // Log the API response data to the console
       console.log("API Response:", response);
 
-      // Password changed successfully, you can redirect or show a success message
       Swal.fire("Password changed!", "Your password has been updated.", "success");
 
-      // Redirect to a different page after a successful password change
       history.push("/");
     } catch (error) {
-      // Handle API request errors
       console.error("API Error:", error);
       Swal.fire("Error", "An error occurred while changing the password.", "error");
     }
@@ -43,31 +43,51 @@ function NewPassword() {
     <div className="NewPassword-container">
       <h2>Reset Password</h2>
       <form onSubmit={handleSubmit}>
-        <div className="input-box">
+        <div className="input-box-password">
           <label htmlFor="password1">New Password</label>
-          <input
-            type="password"
-            id="password1"
-            name="password1"
-            placeholder="Enter your new password"
-            value={formData.password1}
-            onChange={handleInputChange}
-            required
-          />
+          <div className="password-input">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password1"
+              name="password1"
+              placeholder="Enter your new password"
+              value={formData.password1}
+              onChange={handleInputChange}
+              required
+            />
+            {/* Password visibility toggle icon */}
+            <span className="password-toggle-icon" onClick={handlePasswordToggle}>
+              {showPassword ? (
+                <span role="img" aria-label="Hide Password">🙈</span>
+              ) : (
+                <span role="img" aria-label="Show Password">👁️</span>
+              )}
+            </span>
+          </div>
         </div>
-        <div className="input-box">
+        <div className="input-box-password">
           <label htmlFor="password2">Confirm Password</label>
-          <input
-            type="password"
-            id="password2"
-            name="password2"
-            placeholder="Confirm your new password"
-            value={formData.password2}
-            onChange={handleInputChange}
-            required
-          />
+          <div className="password-input">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password2"
+              name="password2"
+              placeholder="Confirm your new password"
+              value={formData.password2}
+              onChange={handleInputChange}
+              required
+            />
+            {/* Password visibility toggle icon */}
+            <span className="password-toggle-icon" onClick={handlePasswordToggle}>
+              {showPassword ? (
+                <span role="img" aria-label="Hide Password">🙈</span>
+              ) : (
+                <span role="img" aria-label="Show Password">👁️</span>
+              )}
+            </span>
+          </div>
         </div>
-        <button type="submit" className="button1">
+        <button type="submit" className="button-forgetpassword">
           Submit
         </button>
       </form>
